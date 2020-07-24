9f057083f642dcf9570a3ce6f691ed29d95657d8
Felipe Carvalho <felipe.carvalho@thoughtworks.com>
2020-07-24 14:35:43 +0200

    Initial project creation

---
+++ src/main/kotlin/com/github/felipecao/honestlogin/HonestLoginApplication.kt
@@ -0,0 +1,11 @@
+package com.github.felipecao.honestlogin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HonestLoginApplication
+
+fun main(args: Array<String>) {
+	runApplication<HonestLoginApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/felipecao/honestlogin/HonestLoginApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.felipecao.honestlogin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HonestLoginApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
